4f4e81031bc590287711bf77bb62c3550e19f0c3
Anatoliy Chakkaev <anatoliy.chakkaev@flatsoft.ru>
2011-05-22 22:39:09 +0400

    Fix yml

--- install.js
+++ install.js
@@ -1,28 +1,28 @@
 var config = [
     'development:',
     '  url: "http://localhost:3000"',
-    '  mailer: sendmail',
+    '  mailer: "sendmail"',
     '  from: "noreply@localhost"',
     'test:',
     '  url: "http://localhost"',
-    '  mailer: test',
+    '  mailer: "test"',
     '  from: "noreply@example.com"',
     'production:',
     '  mailer: smtp',
     '  url: "http://yoursitename.tld"',
     '  from: "noreply@yoursitename.tld"',
-    '  host: localhost',
+    '  host: "localhost"',
     '  port: 25',
     '  use_authentication: 0',
     '  user: ""',
-    '  pass: ""'
+    '  pass: ""\n'
 ].join('\n');
 
 var fs = require('fs');
 var path = require('path');
 
 if (path.existsSync(app.root + '/config') && !path.existsSync(app.root + '/config/mailer.yml')) {
-    fs.writeFileSync(app.root + '/config/mailer.yml', config);
+    fs.writeFileSync(app.root + '/config/mailer.yml', config, 'utf8');
 }
 
 process.exit(0);
