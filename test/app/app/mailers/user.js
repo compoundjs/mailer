9384af242ffaee9a712e1e31f3b0297d7bbda957
Anatoliy Chakkaev <rpm1602@gmail.com>
2013-03-08 01:22:36 +0800

    Added test app

---
+++ test/app/app/mailers/user.js
@@ -0,0 +1,17 @@
+exports.registration = function(user) {
+    this.locals.user = user;
+    this.send({
+        to: user.email,
+        subject: 'Welcome!'
+    });
+};
+
+exports.resetPassword = function(user) {
+    this.layout = false;
+    this.locals.user = user;
+    this.send({
+        to: user.email,
+        subject: 'Reset password request',
+        from: 'compoundjs@example.com'
+    });
+};
